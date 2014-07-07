25f62a5149342030b8c0ebb58333acb89d85ca1b
Thomas Guerin <thomas.guerin2@gmail.com>
2014-07-07 11:20:47 +0200

    Add fluent android bundle sources

---
+++ src/main/java/bundle/FragmentArgsSetter.java
@@ -0,0 +1,11 @@
+package bundle;
+
+import android.app.Fragment;
+
+public class FragmentArgsSetter {
+
+    public static <T extends Fragment> T setFragmentArguments(T fragment, FluentBundle fluentBundle) {
+        fragment.setArguments(fluentBundle.get());
+        return fragment;
+    }
+}
